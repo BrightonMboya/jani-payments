ce21b1b9ed4147061def010944659363f9be8d3f
brightonmboya <b.mboya@alustudent.com>
2025-01-22 18:09:26 +0300

    added cron jobs infra

---
+++ infra/cron.ts
@@ -0,0 +1,35 @@
+import { secrets } from "./secrets";
+
+new sst.aws.Cron("Handles Discount Expires Date", {
+  schedule: "rate(1 day)",
+  job: {
+    handler:
+      "../packages/api/src/routes/discounts/jobs/handle-discount-expires-date",
+    link: [secrets.DATABASE_URL],
+  },
+});
+
+new sst.aws.Cron("Mark Subscription as Paused", {
+  schedule: "rate(1 day)",
+  job: {
+    handler: "../packages/api/src/routes/subscription/jobs/mark-as-paused",
+    link: [secrets.DATABASE_URL],
+  },
+});
+
+new sst.aws.Cron("Subscription Scheduled Changes", {
+  schedule: "rate(1 day)",
+  job: {
+    handler:
+      "../packages/api/src/routes/subscription/jobs/subscription_scheduled_changes",
+    link: [secrets.DATABASE_URL],
+  },
+});
+
+new sst.aws.Cron("Trial Management", {
+  schedule: "rate(1 day)",
+  job: {
+    handler: "../packages/api/src/routes/subscription/jobs/trial-management",
+    link: [secrets.DATABASE_URL],
+  },
+});
